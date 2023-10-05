59d548f80d56bbfbfdde18ace0d8c5f253fe7ed2
olaf.spiewok <olaf.spiewok@endava.com>
2023-10-05 21:10:12 +0200

    add another call method with parameters add txt file for possible documentation

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,10 @@
 void call() {
     echo 'Hello World'
 }
+
+void call(Map args) {
+    echo 'Hello World with arguments'
+    for (arg in args) {
+        println "${arg.key} : ${arg.value}"
+    }
+}
