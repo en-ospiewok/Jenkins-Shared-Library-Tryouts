eacc815966fc9f198cc5a7b1144c242c6c6ccfd9
olaf.spiewok <olaf.spiewok@endava.com>
2023-10-06 09:59:13 +0200

    add subfolder test within vars add a runUAT library for testing runcommand

---
+++ vars/runUAT.groovy
@@ -0,0 +1,59 @@
+boolean BuildCookAndRunUAT(Map args = [:]) {
+    String buildCommandString = ''
+
+    // validate the path of the runUAT batch (optional)
+    String runUATBatchPath = args.get('runUATPath', 'Engine/Build/BatchFiles/RunUAT.bat')
+    if (!runUATBatchPath?.trim()) {
+        echo 'BuildCookAndRunUAT exited with an error: runUATPath cannot be empty!'
+        return false
+    }
+    if (!runUATPath.contains('RunUAT.bat')) {
+        echo 'BuildCookAndRunUAT exited with an error: runUATPath have to contain \"RunUAT.bat\"!'
+        return false
+    }
+
+    buildCommandString += "${runUATBatchPath} BuildCookRun"
+
+    // validate project path (required)
+    String project = args.get('project', null)
+    if (!project?.trim()) {
+        echo 'BuildCookAndRunUAT exited with an error: project argument is required but found empty!'
+        return false
+    }
+
+    buildCommandString += " -project=\"${project}\""
+
+    //target validation (required)
+    String targetArgument = args.get('target')
+    if (!targetArgument?.trim()) {
+        echo 'BuildCookAndRunUAT exited with an error: target argument is required but found empty!'
+        return false
+    }
+
+    // add some fix parameters, which are not parametized atm
+    buildCommandString += ' -build -cook -compile -iterate -stage -package -pak'
+
+    // platform (optional)
+    String platformArgument = args.get('platform', 'Win64')
+    buildCommandString += " -platform=${platformArgument}"
+
+    //clientconfig (optional)
+    String clientConfigArgument = args.get('clientconfig', 'Development')
+    buildCommandString += " -clientconfig=${clientConfigArgument}"
+
+    //clientconfig (optional)
+    String archiveDirectoryArgument = args.get('archivedirectory', "${WORKSPACE}\\Build")
+    buildCommandString += " -archive -archivedirectory=\"${archiveDirectoryArgument}\""
+
+    // execute the batch with all arguments
+    echo "Run command ${buildCommandString}"
+    bat "${buildCommandString}"
+    return true
+}
+
+boolean BuildCookAndRunUAT(String projectPath) {
+    argumentMap = [
+        'projectPath' : projectPath
+    ]
+    return BuildCookAndRunUAT(argumentMap)
+}
