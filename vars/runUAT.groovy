a8523e2c8ec3c930c5e9ca46378daf2573d99d46
olaf.spiewok <olaf.spiewok@endava.com>
2023-10-06 11:19:00 +0200

    try to wrap script and bat together in one pipeline command

--- vars/runUAT.groovy
+++ vars/runUAT.groovy
@@ -1,58 +1,60 @@
 void BuildCookAndRunUAT(Map args = [:]) {
-    String buildCommandString = ''
+    // do a bit of logic in a script block to create the proper build command string
+    script {
+        String buildCommandString = ''
 
-    // validate the path of the runUAT batch (optional)
-    String runUATPathArgument = args.getOrDefault('runUATPath', 'Engine/Build/BatchFiles/RunUAT.bat')
-    if (!runUATPathArgument?.trim()) {
-        currentBuild.result = 'FAILURE'
-        error('BuildCookAndRunUAT exited with an error: runUATPath cannot be empty!')
-        return
+        // validate the path of the runUAT batch (optional)
+        String runUATPathArgument = args.getOrDefault('runUATPath', 'Engine/Build/BatchFiles/RunUAT.bat')
+        if (!runUATPathArgument?.trim()) {
+            currentBuild.result = 'FAILURE'
+            error('BuildCookAndRunUAT exited with an error: runUATPath cannot be empty!')
+            return
+        }
+        if (!runUATPathArgument.contains('RunUAT.bat')) {
+            currentBuild.result = 'FAILURE'
+            error('BuildCookAndRunUAT exited with an error: runUATPath have to contain \"RunUAT.bat\"!')
+            return
+        }
+
+        buildCommandString += "${runUATPathArgument} BuildCookRun"
+
+        // validate project path (required)
+        String project = args.getOrDefault('project', null)
+        if (!project?.trim()) {
+            currentBuild.result = 'FAILURE'
+            error('BuildCookAndRunUAT exited with an error: project argument is required but found empty!')
+            return
+        }
+
+        buildCommandString += " -project=\"${project}\""
+
+        //target validation (required)
+        String targetArgument = args.getOrDefault('target', null)
+        if (!targetArgument?.trim()) {
+            currentBuild.result = 'FAILURE'
+            error('BuildCookAndRunUAT exited with an error: target argument is required but found empty!')
+            return
+        }
+
+        // add some fix parameters, which are not parametized atm
+        buildCommandString += ' -build -cook -compile -iterate -stage -package -pak'
+
+        // platform (optional)
+        String platformArgument = args.getOrDefault('platform', 'Win64')
+        buildCommandString += " -platform=${platformArgument}"
+
+        //clientconfig (optional)
+        String clientConfigArgument = args.getOrDefault('clientconfig', 'Development')
+        buildCommandString += " -clientconfig=${clientConfigArgument}"
+
+        //clientconfig (optional)
+        String archiveDirectoryArgument = args.getOrDefault('archivedirectory', "${WORKSPACE}\\Build")
+        buildCommandString += " -archive -archivedirectory=\"${archiveDirectoryArgument}\""
     }
-    if (!runUATPathArgument.contains('RunUAT.bat')) {
-        currentBuild.result = 'FAILURE'
-        error('BuildCookAndRunUAT exited with an error: runUATPath have to contain \"RunUAT.bat\"!')
-        return
-    }
-
-    buildCommandString += "${runUATPathArgument} BuildCookRun"
-
-    // validate project path (required)
-    String project = args.getOrDefault('project', null)
-    if (!project?.trim()) {
-        currentBuild.result = 'FAILURE'
-        error('BuildCookAndRunUAT exited with an error: project argument is required but found empty!')
-        return
-    }
-
-    buildCommandString += " -project=\"${project}\""
-
-    //target validation (required)
-    String targetArgument = args.getOrDefault('target', null)
-    if (!targetArgument?.trim()) {
-        currentBuild.result = 'FAILURE'
-        error('BuildCookAndRunUAT exited with an error: target argument is required but found empty!')
-        return
-    }
-
-    // add some fix parameters, which are not parametized atm
-    buildCommandString += ' -build -cook -compile -iterate -stage -package -pak'
-
-    // platform (optional)
-    String platformArgument = args.getOrDefault('platform', 'Win64')
-    buildCommandString += " -platform=${platformArgument}"
-
-    //clientconfig (optional)
-    String clientConfigArgument = args.getOrDefault('clientconfig', 'Development')
-    buildCommandString += " -clientconfig=${clientConfigArgument}"
-
-    //clientconfig (optional)
-    String archiveDirectoryArgument = args.getOrDefault('archivedirectory', "${WORKSPACE}\\Build")
-    buildCommandString += " -archive -archivedirectory=\"${archiveDirectoryArgument}\""
 
     // execute the batch with all arguments
     echo "Run command ${buildCommandString}"
     bat "${buildCommandString}"
-    return true
 }
 
 void BuildCookAndRunUAT(String projectPath) {
